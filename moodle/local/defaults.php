940f3c5a874df41b96a7aef5e2714732b0cefc74
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-12-16 14:22:49 -0300

    Update defaults.php

--- moodle/local/defaults.php
+++ moodle/local/defaults.php
@@ -3,4 +3,26 @@
 $defaults['moodle']['timezone'] = 'America/Sao_Paulo';
 $defaults['moodle']['defaultcity'] = 'Curitiba';
 
+$defaults['moodle']['allowuserthemes'] = 'yes';
+$defaults['moodle']['allowcoursethemes'] = 'yes';
+$defaults['moodle']['allowcategorythemes'] = 'yes';
+$defaults['moodle']['allowthemechangeonurl'] = 'yes';
+
+$defaults['moodle']['cronclionly'] = 'no';
+$defaults['moodle']['cronremotepassword'] = 'up7zEp2xHN99wbQk';
+
+$defaults['moodle']['custommenuitems'] = 'Tema
+-Boost | /?theme=boost
+-Clássico | /?theme=classic
+Solicitação de curso | /course/request.php
+Cron | /admin/cron.php?password=up7zEp2xHN99wbQk
+';
+
 $defaults['moodle']['auth_instructions'] = 'admin / fSt@8nNV';
+
+$defaults['moodle']['pathtophp'] = 'C:\moodle-local\server\php\php.exe';
+$defaults['moodle']['pathtogs'] = 'C:\moodle-local\server\gs\bin\gswin64c.exe';
+$defaults['moodle']['pathtopython'] = 'C:\moodle-local\server\python\python.exe';
+$defaults['moodle']['pathtounoconv'] = 'C:\moodle-local\server\unoconv\unoconv.bat';
+
+
