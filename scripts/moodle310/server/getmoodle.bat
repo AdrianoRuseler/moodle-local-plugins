a46f9a362f9847042fddc818695bfcbc54ac81d8
Adriano Ruseler <ruseler@utfpr.edu.br>
2021-01-29 21:24:03 -0300

    Create getmoodle.bat

---
+++ scripts/moodle310/server/getmoodle.bat
@@ -0,0 +1,9 @@
+
+
+curl --output moodle-latest-310.zip --url https://download.moodle.org/download.php/direct/stable310/moodle-latest-310.zip
+
+tar -xf moodle-latest-310.zip
+
+DEL moodle-latest-310.zip
+
+MKDIR moodledata
