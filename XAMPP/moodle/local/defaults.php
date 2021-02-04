9854db966ced29da1860b2066ac95ab9bf8bceba
Adriano Ruseler <ruseler@utfpr.edu.br>
2021-02-04 08:29:53 -0300

    Moving files to acept XAMP and VirtualBox versions

---
+++ XAMPP/moodle/local/defaults.php
@@ -0,0 +1,26 @@
+<?php
+
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+
+$defaults['moodle']['cronclionly'] = 'no';
+$defaults['moodle']['cronremotepassword'] = 'up7zEp2xHN99wbQk';
+
+$defaults['moodle']['custommenuitems'] = 'Tema
+-Boost | /?theme=boost
+-Clássico | /?theme=classic
+Solicitação de curso | /course/request.php
+Cron | /admin/cron.php?password=up7zEp2xHN99wbQk
+';
+
+$defaults['moodle']['auth_instructions'] = 'admin / fSt@8nNV';
+
+$defaults['moodle']['pathtophp'] = 'C:\moodle-local\server\php\php.exe';
+$defaults['moodle']['pathtogs'] = 'C:\moodle-local\server\gs\bin\gswin64c.exe';
+$defaults['moodle']['pathtopython'] = 'C:\moodle-local\server\python\python.exe';
+$defaults['moodle']['pathtodot'] = 'C:\moodle-local\server\Graphviz\bin\dot.exe';
+$defaults['moodle']['pathtodu'] = 'C:\moodle-local\server\du\du.exe';
+
+$defaults['moodle']['pathtounoconv'] = 'C:\moodle-local\server\unoconv\unoconv.bat';
+
+
